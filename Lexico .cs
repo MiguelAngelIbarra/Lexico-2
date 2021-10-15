6dbc97cfe69ac53567a5869bed97950536b433dd
MiguelAngelIbarra <89610156+MiguelAngelIbarra@users.noreply.github.com>
2021-10-15 15:37:53 -0500

    Update Lexico .cs

--- Lexico .cs
+++ Lexico .cs
@@ -4,11 +4,19 @@ using System;
 
 namespace Lexico_2
 { 
-    
+    /*
+        Requerimiento 1: numeros 
+        Requerimiento 2: operadores logicos
+        Requerimiento 3: operadores relaciones
+        Requerimiento 4: operador de termino e incremento de termino 
+        Requerimiento 5: Cadena 
+        Requerimiento 6: Comentarios
+        Requerimiento 7: Excepcion de error
+    */
     public class Lexico:Token 
     {
-        const int  E=-1;//negativo por si crece el automata 
-        const int  F=-2;
+        const int  E=-2;//negativo por si crece el automata 
+        const int  F=-1;
         
         StreamReader archivo;
         StreamWriter log;
@@ -24,7 +32,7 @@ namespace Lexico_2
         }
         public int Automata(int EstadoActual,char Trancision)
         {
-            int sigEstado = 0;
+            int sigEstado = EstadoActual;
             switch(EstadoActual)
             { 
                 case 0:
@@ -35,6 +43,31 @@ namespace Lexico_2
                     else if(char.IsLetter(Trancision))
                     { 
                         sigEstado=1;
+                    }
+                    else if(char.IsDigit(Trancision))
+                    { 
+                        sigEstado = 2;
+                    }
+                    else if(Trancision== '=')
+                    { 
+                        sigEstado=8;
+                    }
+                    else if(Trancision== ':')
+                    { 
+                        sigEstado=10;
+                    }
+                    else if(Trancision== ';')
+                    {
+                        sigEstado=12;
+                    }
+
+                    else if(Trancision== '*'||Trancision== '%')
+                    {
+                        sigEstado=27;
+                    }
+                    else if(Trancision== '?')
+                    {
+                        sigEstado=32;
                     }
                     else
                     {
@@ -50,16 +83,56 @@ namespace Lexico_2
                     break;
 
                 case 2: 
+                    setClasificacion(Tipos.numero);
+                    if(char.IsDigit(Trancision))
+                    {
+                        sigEstado = 2;
+                    }
+                    else
+                    {
+                        sigEstado =F;
+                    }
+                    break;
                 case 3: 
                 case 4: 
                 case 5: 
                 case 6: 
                 case 7: 
                 case 8: 
+                    setClasificacion(Tipos.asignacion);
+                    if(Trancision=='=')
+                    { 
+                        sigEstado=9;
+                    }
+                    else
+                    {
+                        sigEstado =F;
+                    }
+            
+                    break;
                 case 9: 
-                case 10: 
-                case 11: 
-                case 12: 
+                    setClasificacion(Tipos.opRelacional);
+                    sigEstado=F;
+                    break;
+                case 10:
+                    setClasificacion(Tipos.caracter);
+                    if(Trancision=='=')
+                    { 
+                        sigEstado=11;
+                    } 
+                    else
+                    {
+                        sigEstado=F;
+                    }
+                    break;
+                case 11:
+                    setClasificacion(Tipos.inicializacion);
+                    sigEstado=F;
+                    break; 
+                case 12:
+                    setClasificacion(Tipos.finSentencia); 
+                    sigEstado=F;
+                    break;
                 case 13: 
                 case 14: 
                 case 15: 
@@ -74,12 +147,28 @@ namespace Lexico_2
                 case 24: 
                 case 25: 
                 case 26: 
-                case 27: 
-                case 28: 
+                case 27:
+                    setClasificacion(Tipos.opFactor);
+                    if(Trancision=='=')
+                    { 
+                        sigEstado=28;
+                    }
+                    else
+                    {
+                        sigEstado=F;
+                    }
+                    break;
+                case 28:
+                    setClasificacion(Tipos.incFactor);
+                    sigEstado=F;
+                    break; 
                 case 29: 
                 case 30: 
                 case 31: 
-                case 32: 
+                case 32:
+                    setClasificacion(Tipos.ternario);
+                    sigEstado=F;
+                    break; 
                 case 33:
                     setClasificacion(Tipos.caracter);
                     sigEstado=F;
@@ -107,7 +196,7 @@ namespace Lexico_2
             while(Estado>=0)
             {
                 c=(char)archivo.Peek();
-                Estado=Automata(Estado,c);
+                Estado=Automata(Estado,c); 
                 if(Estado>=0)
                 {
                     archivo.Read();
@@ -117,6 +206,10 @@ namespace Lexico_2
                     }
                 }
 
+            }
+            if(Estado==E)
+            {
+                //levantar excepcion correspondiente
             }
 
             
